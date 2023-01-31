7465667a1746962b3f458a9a7885fe6beda500ad
olegrewko <olegrewko@gmail.com>
2023-01-31 11:47:03 +0300

    q

--- src/js/main.js
+++ src/js/main.js
@@ -147,5 +147,23 @@ $(function () {
 
   const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
   initializeClock('promo__clock', deadline);
-
 });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 40.716, lng: -74.003 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
+

e61dccd5d61299748ae6ab581ee0540d7d9ce2d3
olegrewko <olegrewko@gmail.com>
2023-02-10 11:38:15 +0300

    y

--- src/js/main.js
+++ src/js/main.js
@@ -151,14 +151,14 @@ $(function () {
 
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
+
   const uluru = { lat: 40.716, lng: -74.003 };
-  // The map, centered at Uluru
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: uluru,
   });
-  // The marker, positioned at Uluru
+
   const marker = new google.maps.Marker({
     position: uluru,
     map: map,

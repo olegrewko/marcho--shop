5adb4b2a20f487baafcc22a288a32258003c51c7
olegrewko <olegrewko@gmail.com>
2023-04-02 11:24:18 +0300

    end

--- gulpfile.js
+++ gulpfile.js
@@ -147,29 +147,7 @@ const images = () => {
     ])
 
     .pipe(gulpif(isProd, image()))
-    // .pipe(imagemin(
-    //   [
-    //     imagemin.gifsicle({
-    //       interlaced: true
-    //     }),
-    //     imagemin.mozjpeg({
-    //       quality: 75,
-    //       progressive: true
-    //     }),
-    //     imagemin.optipng({
-    //       optimizationLevel: 5
-    //     }),
-    //     imagemin.svgo({
-    //       plugins: [{
-    //           removeViewBox: true
-    //         },
-    //         {
-    //           cleanupIDs: false
-    //         }
-    //       ]
-    //     })
-    //   ]
-    // ))
+  
     .pipe(dest('./app/img'))
 };
 
